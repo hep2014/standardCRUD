3ba3605af6c1ff563328d46dda54e112531d339c
Ilya Alexandrovich Matveev <409097@niuitmo.ru>
2025-11-14 13:12:04 +0300

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.db.db import Base
-from app.models.user import User
-from app.models.news import News
-from app.models.comment import Comment
-
-__all__ = ["Base", "User", "News", "Comment"]
