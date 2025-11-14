e669c6c97338ece5dbdcc740b852d55ece2135b8
Ilya Alexandrovich Matveev <409097@niuitmo.ru>
2025-11-14 13:20:47 +0300

    Add files via upload

---
+++ app/db.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import redis
+import os
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=NAMING_CONVENTION)
+
+# В строке происходит подключение к локальной базе данных, расположенной на 5432, название базы - ilya_news_db
+DATABASE_URL = "postgresql+psycopg2://postgres:75918Qn@localhost:5432/ilya_news_db"
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+engine = create_engine(DATABASE_URL, future=True)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
